5c4e1979fe81482f60be929683ac954deb0f927d
Abhinav Nath <abhinavnath@ymail.com>
2022-07-11 23:29:49 +0530

    Initialise project

---
+++ src/main/java/com/codecafe/postgres/WorkingWithPostgresJsonbApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithPostgresJsonbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorkingWithPostgresJsonbApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/codecafe/postgres/WorkingWithPostgresJsonbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecafe.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithPostgresJsonbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
